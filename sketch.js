4341115af302afd61bcae343e41cb4299e1180ad
Lucas Pinheiro <lucaspinheiroifsul@gmail.com>
2020-05-16 10:42:52 -0300

    add tips

--- sketch.js
+++ sketch.js
@@ -1,112 +1,108 @@
-var btn_cima = document.querySelector("#btn-cima");
-var btn_baixo = document.querySelector("#btn-baixo");
-var btn_esquerda = document.querySelector("#btn-esquerda");
-var btn_direita = document.querySelector("#btn-direita");
+var btn_cima = document.querySelector("#btn-cima")
+var btn_baixo = document.querySelector("#btn-baixo")
+var btn_esquerda = document.querySelector("#btn-esquerda")
+var btn_direita = document.querySelector("#btn-direita")
 
-
-var paused = false;
-var snake;
-var scl = 20;
-var food;
-var score = 0;
+var paused = false
+var snake
+var scl = 20
+var food
+var score = 0
 
 function setup() {
-  canvas = createCanvas(600, 440);
-  canvas.parent('sketch-holder');
-  frameRate(12);
-  snake = new Snake();
-  pickLocation();
+  canvas = createCanvas(600, 440)
+  canvas.parent("sketch-holder")
+  frameRate(12)
+  snake = new Snake()
+  pickLocation()
 }
 
 function draw() {
   if (!paused) {
-    background(86, 145, 68);
+    background(86, 145, 68)
 
     if (snake.eat(food)) {
-      pickLocation();
-      snake.life = 500;
-      score += 50;
+      pickLocation()
+      snake.life = 500
+      score += 50
     }
     if (snake.die()) {
-      print("Snake died")
-      score = 0;
+      score = 0
     }
-    snake.update();
-    snake.show();
+    snake.update()
+    snake.show()
 
-    fill(255, 0, 0);
+    fill(255, 0, 0)
     stroke(0)
-    rect(food.x, food.y, scl, scl);
-    updateScore();
+    rect(food.x, food.y, scl, scl)
+    updateScore()
   }
 }
 
 function updateScore() {
   fill(0)
-  textSize(24);
-  textAlign(RIGHT);
-  text("Score: " + score, width - 24, height - 24);
+  textSize(24)
+  textAlign(RIGHT)
+  text("Score: " + score, width - 24, height - 24)
 }
 
-
 function pickLocation() {
-  var cols = floor(width / scl);
-  var rows = floor(height / scl);
-  food = createVector(floor(random(cols)), floor(random(rows)));
-  food.mult(scl);
+  var cols = floor(width / scl)
+  var rows = floor(height / scl)
+  food = createVector(floor(random(cols)), floor(random(rows)))
+  food.mult(scl)
 }
 
 function keyPressed() {
-
   switch (keyCode) {
     case 87: //CIMA
-      moveUp();
-      buttonFocus(btn_cima);
-      break;
+      moveUp()
+      buttonFocus(btn_cima)
+      break
     case 68: //DIREITA
-      moveRight();
-      buttonFocus(btn_direita);
-      break;
+      moveRight()
+      buttonFocus(btn_direita)
+      break
     case 83: //BAIXO
-      moveDown();
-      buttonFocus(btn_baixo);
-      break;
+      moveDown()
+      buttonFocus(btn_baixo)
+      break
     case 65: //ESQUERDA
-      moveLeft();
-      buttonFocus(btn_esquerda);
-      break;
+      moveLeft()
+      buttonFocus(btn_esquerda)
+      break
     case 32:
-      paused = !paused;
-      break;
+      paused = !paused
+      break
     case 82:
-      paused = false;
-      snake.life = 500;
-      score = 0;
-      break;
+      paused = false
+      snake.life = 500
+      score = 0
+      break
   }
 }
 
 function buttonFocus(button) {
-  btn_cima.blur();
-  btn_baixo.blur();
-  btn_esquerda.blur();
-  btn_direita.blur();
-  button.focus();
+  btn_cima.blur()
+  btn_baixo.blur()
+  btn_esquerda.blur()
+  btn_direita.blur()
+  button.focus()
 }
 
 function moveUp() {
-  if (snake.yspeed == 1 && snake.total != 0) return;
-  snake.dir(0, -1);
+  if (snake.yspeed == 1 && snake.total != 0) return
+  snake.dir(0, -1)
 }
 function moveDown() {
-  if (snake.yspeed == -1 && snake.total != 0) return;
-  snake.dir(0, 1);
+  if (snake.yspeed == -1 && snake.total != 0) return
+  snake.dir(0, 1)
 }
 function moveRight() {
-  if (snake.xspeed == -1 && snake.total != 0) return;
-  snake.dir(1, 0);
+  if (snake.xspeed == -1 && snake.total != 0) return
+  snake.dir(1, 0)
 }
 function moveLeft() {
-  if (snake.xspeed == 1 && snake.total != 0) return;
-  snake.dir(-1, 0);
+  if (snake.xspeed == 1 && snake.total != 0) return
+  snake.dir(-1, 0)
 }
--- snake.js
+++ snake.js
@@ -1,44 +1,40 @@
 class Snake {
-
   constructor() {
-    this.x = 20;
-    this.y = 20;
-    this.xspeed = 1;
-    this.yspeed = 0;
-    this.tail = [];
-    this.total = 0;
-    this.life = 500;
+    this.x = 20
+    this.y = 20
+    this.xspeed = 1
+    this.yspeed = 0
+    this.tail = []
+    this.total = 0
+    this.life = 500
   }
 
-
   update() {
-    this.life -= 5;
+    this.life -= 5
 
     if (this.total === this.tail.length) {
       for (let i = 0; i < this.tail.length - 1; i++) {
         this.tail[i] = this.tail[i + 1]
       }
     }
-    this.tail[this.total - 1] = createVector(this.x, this.y);
+    this.tail[this.total - 1] = createVector(this.x, this.y)
 
-    this.x += this.xspeed * scl;
-    this.y += this.yspeed * scl;
+    this.x += this.xspeed * scl
+    this.y += this.yspeed * scl
 
     //Se a cobra sair da tela na direita, deverá reaparecer na esquerda
     //E assim por diante em todos os cantos da tela
     if (this.x > width) {
-      print("saiu")
-      this.x = 0;
+      this.x = 0
     }
     if (this.x < 0) {
-      print("saiu")
-      this.x = width;
+      this.x = width
     }
     if (this.y > height) {
-      this.y = 0;
+      this.y = 0
     }
     if (this.y < 0) {
-      this.y = height;
+      this.y = height
     }
 
     //this.x = constrain(this.x, 0, width - scl);
@@ -48,15 +44,15 @@ class Snake {
   eat(food) {
     var d = dist(this.x, this.y, food.x, food.y)
     if (d < 2) {
-      this.total++;
-      return true;
+      this.total++
+      return true
     }
-    return false;
+    return false
   }
 
   show() {
-    fill(0, 0, 0, this.life);
-    stroke(100, 100, 100, this.life);
+    fill(0, 0, 0, this.life)
+    stroke(100, 100, 100, this.life)
     rect(this.x, this.y, scl, scl)
 
     for (let i = 0; i < this.tail.length; i++) {
@@ -65,24 +61,22 @@ class Snake {
   }
 
   die() {
-    if(this.life < 0 ) return true;
+    if (this.life < 0) return true
 
     for (let i = 0; i < this.tail.length; i++) {
-      let pos = this.tail[i];
-      let distancia = dist(this.x, this.y, pos.x, pos.y);
+      let pos = this.tail[i]
+      let distancia = dist(this.x, this.y, pos.x, pos.y)
       if (distancia < 1) {
-        this.total = 0;
-        this.tail = [];
-        return true;
+        this.total = 0
+        this.tail = []
+        return true
       }
     }
-    return false;
+    return false
   }
 
   dir(x, y) {
-    this.xspeed = x;
-    this.yspeed = y;
+    this.xspeed = x
+    this.yspeed = y
   }
-
-
 }
